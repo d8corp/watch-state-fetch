13fcceddd25d2e5a9b5fe5726e8ccd91e014aae2
Mike Lysikov <d8corp@mail.ru>
2023-06-09 23:10:10 +0300

    changes for v2.0

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,6 @@
-import Fetch from '.'
 import 'isomorphic-fetch'
+
+import Fetch from '.'
 
 const user1 = fetch('https://reqres.in/api/users/1').then(data => data.json())
 
@@ -29,40 +30,32 @@ describe('Ajax', () => {
     const user = new Fetch('https://reqres.in/api/users/1')
     let f, t, c
 
-    user
-      .finally(value => f = value)
-      .then(value => t = value)
-      .catch(value => c = value)
-
     await user
+      .then(value => {
+        t = value
+      })
+      .catch(value => {
+        c = value
+      })
+      .finally(() => {
+        f = true
+      })
 
-    expect(f).toEqual(await user1)
+    expect(f).toEqual(true)
     expect(t).toEqual(await user1)
     expect(c).toBe(undefined)
   })
   test('error', async () => {
-    const user = new Fetch('https://reqres.in/api/login', {method: 'post', type: "json"})
+    const user = new Fetch('https://reqres.in/api/login', { method: 'post', type: 'json' })
     try { await user } catch (e) {}
     expect(user.value).toBe(undefined)
-    expect(user.error).toEqual({error: 'Missing email or username'})
+    expect(user.error).toEqual({ error: 'Missing email or username' })
   })
   test('answer', async () => {
     const user = new Fetch('https://reqres.in/api/users/23')
-    expect(user.answer).toBe(undefined)
+    expect(user.response).toBe(undefined)
     try { await user } catch (e) {}
-    expect(user.answer.ok).toBe(false)
-    expect(user.answer.status).toBe(404)
-  })
-  test('resolve', async () => {
-    const user = new Fetch('https://reqres.in/api/users/1')
-    user.resolve({data: {id: 2}})
-    await user
-    expect(user.value.data.id).toBe(2)
-
-    user.update()
-
-    await user
-
-    expect(user.value.data.id).toBe(1)
+    expect(user.response.ok).toBe(false)
+    expect(user.response.status).toBe(404)
   })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,33 @@
-import Async, { IAsyncOptions } from '@watch-state/async'
-import { State, globalEvent } from 'watch-state'
+import Async from '@watch-state/async'
+import { event, state } from '@watch-state/decorators'
 
-export interface FetchOptions<V = any, E = any> extends RequestInit, Omit<IAsyncOptions<V, E>, 'request'> {
+export interface FetchOptions<V = any> extends RequestInit {
   type?: 'json' | 'text' | 'blob' | 'arrayBuffer' | 'formData'
+  defaultValue?: V
 }
 
-export default class Fetch<V = any, E = any> extends Async<V, E> {
-  _answer = new State()
-  get answer () {
-    return this._answer.value
-  }
-  set answer (value) {
-    this._answer.value = value
+export default class Fetch<V, E = unknown> extends Async<V, E> {
+  @state accessor #response: Response
+  private _response: Response
+  get response () {
+    return this.#response
   }
 
-  constructor (url: string, options: FetchOptions<V, E> = {}, update?: boolean) {
-    super({...options, request: (resolve, reject) => {
-      const { type = 'json' } = options
-      let answer
-      fetch(url, options).then(data => {
-        answer = data
-        return data.status === 204 ? undefined : data[type]()
-      }).then(data => {
-        globalEvent.start()
-        this.answer = answer
-        answer.status > 399 ? reject(data) : resolve(data)
-        globalEvent.end()
-      }, e => {
-        globalEvent.start()
-        this.answer = answer
-        reject(e)
-        globalEvent.end()
-      })
-    }}, update)
+  constructor (url: RequestInfo | URL, options: FetchOptions<V> = {}) {
+    super(() => fetch(url, options).then(response => {
+      this._response = response
+
+      return response.status === 204 ? undefined : response[options.type || 'json']()
+    }), options.defaultValue)
+  }
+
+  @event protected resolve (value: V | E) {
+    this.#response = this._response
+
+    if (this._response.status > 399) {
+      return super.reject(value as E)
+    }
+
+    return super.resolve(value as V)
   }
 }
